a690658374a822b0bc6a2ef8d00e3fb9cb763aec
Garrett Swann <garrettswann3049@yahoo.com>
2017-10-06 01:06:34 -0700

    Working on parser.txt

--- wiki-scraper/parser.py
+++ wiki-scraper/parser.py
@@ -1,47 +1,83 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib 
 
-pageURI = 'https://en.wikipedia.org/wiki/'
+pageURI = 'https://en.wikipedia.org'
 
 if __name__ == '__main__':
 
-    _toVisit = ['Volbeat', 'Red_Hot_Chili_Peppers']
+    _toVisit = [
+            #'/wiki/Ice_Cube',
+            #'/wiki/Audioslave',
+            '/wiki/Volbeat',
+            #'/wiki/Red_Hot_Chili_Peppers',
+            ]
+
 
     _visited = []
     Bands = {}
 
     while len(_toVisit):
-        r = requests.get(pageURI + _toVisit[0] + '_(band)')
+        bandRow={}
+        print('Visiting ', _toVisit[0])
+        bandRow['Band']=_toVisit[0]
+        r = requests.get(pageURI+_toVisit[0])
         soup = BeautifulSoup(r.text, 'lxml')
-
-        table = soup.find('table', attrs={'class':'infobox vcard plainlist'})
-        content_rows = table.find_all('tr')[3:]
-        headings = {}
-        for tr in content_rows:
-            index = ''
-            for th in tr.find_all('th'):
-                if th.get_text() != '':
-                    index = th.get_text()
-                    headings[index] = []
-            if index != '':
-                li = tr.find_all('li')
-                if len(li) != 0:
-                    for i in li:
-                        headings[index].append(i.get_text())
+        
+        try:
+            table = soup.find('table', attrs={'class':'infobox vcard plainlist'})
+            if table is None:
+                table = soup.find('table', attrs={'class':'infobox biography vcard'})
+            content_rows = table.find_all('tr')
+        except:
+            print("This page doesn't have a cheatsheet")
+            _visited.append(_toVisit[0])
+            del _toVisit[0]
+            continue
+        
+        cur_th = ''
+        for each in content_rows:
+            #print(each)
+            #print(each.text)
+            pair={}
+            try_href = False
+            if each.th and each.th.text != '':
+                print(each.th.text)
+                cur_th = each.th.text
+                bandRow[cur_th]=[]
+                #if each.td:
+                    #print("tasda",each.td.text)
+                    #bandRow[cur_th].append(each.td.text)
+            if each.ul:
+                for li in each.ul:
+                    if li == '\n':
+                        continue
+                    text = li.a.text if li.a else li.text
+                    a_value = soup.find(lambda tag: (tag.name == 'a' 
+                        and text == tag.text),href=True)
+                    if a_value:
+                        pair[text]=a_value['href']
+                    else:
+                        pair[text]=None
+                    bandRow[cur_th].append({'Value':text, 'Link':pair[text]})
+            elif each.a:
+                a_value = soup.find(lambda tag: (tag.name == 'a' 
+                    and each.a.text == tag.text),href=True)
+                if a_value:
+                    pair[each.a.text]=a_value['href']
                 else:
-                    headings[index].append(tr.find('td').get_text())
+                    pair[each.a.text]=None
+                print('\t',each.a.text, '----', pair[each.a.text])
+                bandRow[cur_th].append({'Value':each.a.text, 'Link':pair[each.a.text]})
 
-        Bands[_toVisit[0]] = headings
         _visited.append(_toVisit[0])
         del _toVisit[0]
 
-        print(headings)
 
-        print(headings['Associated acts'])
-        for act in headings['Associated acts']:
-            print(act)
-            if act not in _visited:
-                print('Need to visit {}'.format(act))
-                _toVisit.append(act)
-                
-            
+        if bandRow.get('Associated acts', None) and bandRow.get('Genres', None):
+            for act in bandRow['Associated acts']:
+                if act['Link'] not in _visited and act['Link'] is not None:
+                    print('\tNeed to visit {}'.format(act['Value']))
+                    _toVisit.append(act['Link'])
+                elif act['Link'] is None:
+                    print("Can't visit", act['Link'])
