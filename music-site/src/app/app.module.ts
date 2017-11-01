25d5179f0c81aef7947b9eabd01d5c184ccead35
mjschimm <mjschimm@gmail.com>
2017-10-31 19:03:16 -0700

    First change to routes through Angular

--- music-site/src/app/app.module.ts
+++ music-site/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { IndexView } from './app.component';
+import { LoginComponent } from './login.component';
 
 import {HttpClientModule} from '@angular/common/http';
 
-@NgModule({
-    declarations: [
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({ 
+    declarations: [ 
         IndexView,
+        LoginComponent,
     ],
     imports: [
+        AppRoutingModule,
         BrowserModule,
         // Include it under 'imports' in your application module
         // after BrowserModule.
         HttpClientModule,
     ],
+    exports: [ RouterModule ],
     providers: [],
     bootstrap: [IndexView]
 })
---
+++ music-site/src/app/login.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html'
+})
+
+export class LoginComponent implements OnInit { 
+    ngOnInit() {}
+}
