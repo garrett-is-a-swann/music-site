f35533e34609b4c691b4ab7c99a6a3ae3ab53b0d
mjschimm <mjschimm@gmail.com>
2017-12-07 20:02:07 -0800

    Graph is working with basic pre-loaded JSON data

--- music-site/src/app/index.component.ts
+++ music-site/src/app/index.component.ts
@@ -26,67 +26,42 @@ export class IndexView implements OnInit, AfterViewInit{
 
     typed = ''
 
-    onKey(value: string, isClicked: boolean) { // without type info
-        this.typed = value;
-        console.log(isClicked);
-        if( isClicked ) {
-            this.http.post('/api/bands',
-                // Build POST to send
-                {
-                    name:this.typed
-                })
-            .subscribe(); // Sends POST
-        }
-    }
+    cy = undefined;
 
+    JSONARRAY = [["Ataxia","experimental rock|post-punk|psychedelic rock|electronica|art rock"],
+        ["Audioslave","alternative metal|post-grunge|alternative rock|hard rock"],
+        ["Axis of Justice","alternative rock"],
+        ["Dominus","death metal"],
+        ["Future User","electronic rock"],
+        ["Jane's Addiction","funk metal|neo-psychedelia|alternative metal|alternative rock|psychedelic rock"],
+        ["John Frusciante","rock"],
+        ["One Day as a Lion","rap rock"],
+        ["Prophets of Rage","rap metal|rap rock"],
+        ["Rage Against the Machine","rap rock|alternative metal|funk metal|rap metal|nu metal"],
+        ["Red Hot Chili Peppers","alternative rock|funk rock|rap rock|funk metal"],
+        ["Soundgarden","hard rock|alternative metal|alternative rock|heavy metal|grunge"],
+        ["Street Sweeper Social Club","funk rock|hard rock|rap rock"],
+        ["System of a Down","nu metal|hard rock|progressive metal|alternative metal"],
+        ["Temple of the Dog","grunge"],
+        ["The Last Internationale","alternative rock"],
+        ["The Mars Volta","progressive rock|experimental rock"],
+        ["The Nightwatchman","folk rock"],
+        ["Volbeat","psychobilly|heavy metal|hard rock"],
+        ["WAKRAT","hard rock|alternative rock|punk rock"],
+        ["fuck", "fucking rock n' roll, bitch"]];
+
+    onKey(enter) {
+        /*for(var i = 0; i < this.JSONARRAY.length; i++) {
+            var obj = this.JSONARRAY[i];
+            console.log(obj[0]);
+        }*/
+    }
+    
 
     // Init Cytoscape with Jquery
     ngAfterViewInit(): void {
-       cytoscape({
+       this.cy = cytoscape({
             container: document.getElementById('cy'), // container to render in
-            elements: [
-                { data: { id: 'a' } },
-                { data: { id: 'b' } },
-                { data: { id: 'c' } },
-                { data: { id: 'd' } },
-                { data: { id: 'e' } },
-                { data: { id: 'f' } },
-                {
-                    data: {
-                        id: 'ab',
-                        source: 'a',
-                        target: 'b'
-                    }
-                },
-                {
-                    data: {
-                        id: 'cd',
-                        source: 'c',
-                        target: 'd'
-                    }
-                },
-                {
-                    data: {
-                        id: 'ef',
-                        source: 'e',
-                        target: 'f'
-                    }
-                },
-                {
-                    data: {
-                        id: 'ac',
-                        source: 'a',
-                        target: 'c'
-                    }
-                },
-                {
-                    data: {
-                        id: 'be',
-                        source: 'b',
-                        target: 'e'
-                    }
-                }
-            ],
             layout: {
                 name: 'cose'
             },
@@ -102,6 +77,239 @@ export class IndexView implements OnInit, AfterViewInit{
                     }
                 }]
         });
+
     };
 
+    myFunction(input) {
+
+        //FINDS BAND FROM JSON OBJECT
+        var band_exists = false;
+        for(var i = 0; i < this.JSONARRAY.length; i++) {
+            var obj = this.JSONARRAY[i];
+            var genres_string = "";
+            if(input == obj[0]) {
+                band_exists = true;
+                console.log(obj[0]);
+                genres_string = obj[1];
+                break;
+            }
+        }
+        if(band_exists == false) {
+            console.log("Band \""+input+"\" does not exist.");
+            return;
+        }
+
+        //PARSES GENRE STRINGS FROM BAND AND LOADS THEM INTO INTO ARRAY 'genres'
+        var genres = genres_string.split('|');
+
+        //IF THERE IS ONLY ONE GENRE ASSOCIATED WITH THE BAND...
+        if(genres.length == 1) {
+            var node_name = genres[0];
+            if(this.cy.$id(node_name).isNode()) {
+                var node = this.cy.$id(node_name);
+                var w = parseInt(node.renderedStyle('width'));
+                node.style('width', (w+10)+'%');
+                node.style('height', (w+10)+'%');
+                //node.style('padding', (parseInt(node.renderedStyle('padding'))+10)+'px')
+            }
+            else {
+                this.cy.add({
+                data: { id: node_name },
+                style: {'background-color': 'green',
+                        'height': '65%',
+                        'width': '65%',
+                        'font-weight': '500',
+                        'background-opacity': '.55',
+                        'border-color': 'green',
+                        'border-width': '3px'}
+                });
+            }
+            this.cy.fit();
+            var dist_node = this.cy.$id('9');
+            dist_node.data('parent', 'a');
+            this.cy.layout({
+                name: 'cose'
+            }).run();
+        }
+        else {
+        
+            for(var i=0; i<genres.length-1; i++) {
+                if(i == genres.length) {
+                    if(i == 0) {
+
+                    }
+                    return;
+                }
+                else {
+                    var genre = genres[i]
+                    console.log(i);
+                    for(var j=i+1; j<genres.length; j++) {
+                        var a_node_name = genres[i];
+                        var b_node_name = genres[j];
+                       // console.log(a_node_name + "/" + b_node_name);
+                       if(this.cy.$id(a_node_name).isNode() && j == i+1 && i == 0) {
+                            var node = this.cy.$id(a_node_name);
+                            var w = parseInt(node.renderedStyle('width'));
+                            var fs = parseInt(node.renderedStyle('font-size'));
+                            node.style('width', (w+15)+'%');
+                            node.style('height', (w+15)+'%');
+                            node.style('font-size', fs+3+'%');
+                        }
+                        else {
+                            if(!this.cy.$id(a_node_name).isNode()) {
+                                this.cy.add({
+                                data: { id: a_node_name },
+                                style: {'background-color': 'green',
+                                        'height': '65%',
+                                        'width': '65%',
+                                        'font-size': '10%',
+                                        'font-weight': '500',
+                                        'background-opacity': '.55',
+                                        'border-color': 'green',
+                                        'border-width': '3px'}
+                                });
+                            }
+                        }
+                        if(this.cy.$id(b_node_name).isNode() && i == 0) {
+                            var node = this.cy.$id(b_node_name);
+                            var w = parseInt(node.renderedStyle('width'));
+                            var fs = parseInt(node.renderedStyle('font-size'));
+                            node.style('width', (w+15)+'%');
+                            node.style('height', (w+15)+'%');
+                            node.style('font-size', fs+3+'%');
+                        }
+                        else {
+                            if(!this.cy.$id(b_node_name).isNode()) {
+                                this.cy.add({
+                                data: { id: b_node_name },
+                                style: {'background-color': 'green',
+                                        'height': '65%',
+                                        'width': '65%',
+                                        'font-size': '10%',
+                                        'font-weight': '500',
+                                        'background-opacity': '.55',
+                                        'border-color': 'green',
+                                        'border-width': '3px'}
+                                });
+                            }
+                        }
+                        this.cy.fit();
+
+                        if(this.cy.$id(a_node_name+"/"+b_node_name).isEdge() == false && 
+                            this.cy.$id(b_node_name+"/"+a_node_name).isEdge() == false) {
+                         //   alert("shit do not exist");
+                            this.cy.add({
+                                data: {
+                                    id: a_node_name + '/' + b_node_name,
+                                    source: a_node_name,
+                                    target: b_node_name,
+                                    style: {'source-distance-from-node': '1000px',
+                                        'target-endpoint': '2px'
+                                    }
+                                }
+                            });
+                        }
+                        this.cy.layout({
+                            name: 'cose'
+                        }).run();
+
+
+                    }
+                }
+                
+            }
+        }
+
+        /*JUST ADDING NODE A
+        if(b_node_name == "" && a_node_name != "") {
+            if(this.cy.$id(a_node_name).isNode()) {
+                var node = this.cy.$id(a_node_name);
+                var w = parseInt(node.renderedStyle('width'));
+                node.style('width', (w+10)+'%');
+                node.style('height', (w+10)+'%');
+                node.style('padding', (parseInt(node.renderedStyle('padding'))+10)+'px')
+            }
+            else {
+                this.cy.add({
+                data: { id: a_node_name },
+                style: {'background-color': 'green',
+                        'height': '65%',
+                        'width': '65%'}
+                });
+            }
+            this.cy.fit();
+            var dist_node = this.cy.$id('9');
+            dist_node.data('parent', 'a');
+           // alert(dist_node.isParent());
+        }
+
+        /*JUST ADDING NODE B
+        if(a_node_name == "" && b_node_name != "") {
+            if(this.cy.$id(b_node_name).isNode()) {
+                var node = this.cy.$id(b_node_name);
+                var w = parseInt(node.renderedStyle('width'));
+                node.style('width', (w+10)+'%');
+                node.style('height', (w+10)+'%');
+            }
+            else {
+                this.cy.add({
+                data: { id: b_node_name },
+                style: {'background-color': 'green',
+                        'height': '65%',
+                        'width': '65%'}
+                });
+            }
+            this.cy.fit();
+        }
+
+        /*ADDING BOTH NODE A AND B WITH EDGE(A,B)
+        if(a_node_name != "" && b_node_name != "") {
+            if(this.cy.$id(a_node_name).isNode()) {
+                var node = this.cy.$id(a_node_name);
+                var w = parseInt(node.renderedStyle('width'));
+                node.style('width', (w+10)+'%');
+                node.style('height', (w+10)+'%');
+            }
+            else {
+                this.cy.add({
+                data: { id: a_node_name },
+                style: {'background-color': 'green',
+                        'height': '65%',
+                        'width': '65%'}
+                });
+            }
+            if(this.cy.$id(b_node_name).isNode()) {
+                var node = this.cy.$id(b_node_name);
+                var w = parseInt(node.renderedStyle('width'));
+                node.style('width', (w+10)+'%');
+                node.style('height', (w+10)+'%');
+            }
+            else {
+                this.cy.add({
+                data: { id: b_node_name },
+                style: {'background-color': 'green',
+                        'height': '65%',
+                        'width': '65%'}
+                });
+            }
+            this.cy.fit();
+
+            if(this.cy.$id(a_node_name+"/"+b_node_name).isEdge() == false && 
+                this.cy.$id(b_node_name+"/"+a_node_name).isEdge() == false) {
+             //   alert("shit do not exist");
+                this.cy.add({
+                    data: {
+                        id: a_node_name + '/' + b_node_name,
+                        source: a_node_name,
+                        target: b_node_name
+                    }
+                });
+            }
+        }*/
+
+
+    }
+
 }
+
+
