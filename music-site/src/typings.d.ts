1ae6670f8c289f04370c74e1d1990fea2b2a6d8b
Garrett Swann <garrettswann3049@yahoo.com>
2017-12-08 00:27:46 -0800

    Added cytoscape plugin to project and fixed errors

--- music-site/src/typings.d.ts
+++ music-site/src/typings.d.ts
@@ -1,6 +1,7 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
 declare var cytoscape:any;
+declare var cose-bilkent:any; // Probably don't need
 interface NodeModule {
   id: string;
 }
